075a6e93f7eaaf2ca97d15fc49aed6aebed0c1b3
Aksh Sood <sood_aksh_2001@outlook.com>
2022-07-31 11:36:49 +0530

    home start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./Header.js";
+import Home from "./Home.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Home/> */}
+      <Home/>
    
     </div>
   );
